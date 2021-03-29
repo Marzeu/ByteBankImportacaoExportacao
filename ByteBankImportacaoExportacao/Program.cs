640699c4efb22704165906bce78f05faa5daa12c
Marzeu <marzahell@hotmail.com>
2021-03-29 15:47:53 -0300

    buffers

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -22,7 +22,9 @@ namespace ByteBankImportacaoExportacao
                 while (numeroDeBytesLidos != 0)
                 {
                     numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-                    EscreverBuffer(buffer);
+
+
+                    EscreverBuffer(buffer, numeroDeBytesLidos);
                 }
 
 
@@ -31,11 +33,11 @@ namespace ByteBankImportacaoExportacao
             Console.ReadLine();
         }
 
-        static void EscreverBuffer(byte[] buffer)
+        static void EscreverBuffer(byte[] buffer, int bytesLidos)
         {
             var utf8 = new UTF8Encoding();
 
-            var texto = utf8.GetString(buffer);
+            var texto = utf8.GetString(buffer, 0, bytesLidos);
             Console.Write(texto);
 
             //foreach (var meuByte in buffer)
