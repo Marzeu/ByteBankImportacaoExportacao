231e90ec9715cf9c9906a656d2ceba4ea8350b42
Marzeu <marzahell@hotmail.com>
2021-03-29 16:59:36 -0300

    criando CSV

---
+++ ByteBankImportacaoExportacao/3_CriandoArquivos.cs
@@ -0,0 +1,28 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void CriarArquivo()
+        {
+            var caminhoNovoArquivo = "contasExportadas.csv";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            {
+                var contaComoString = "456,78945,57683.89,Gustavo Santos";
+                var enconding = Encoding.UTF8;
+
+                var bytes = enconding.GetBytes(contaComoString);
+
+                fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+            }
+        }        
+    }
+}
